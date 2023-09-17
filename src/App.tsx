5c71e2fcf3289d802cbbebe98256f770adf5bb2a
minus3theta <yone_j_synthesis@yahoo.co.jp>
2023-09-18 03:21:55 +0900

    Run bitris_commands example

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,18 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
-import init, { inc } from 'wasm'
+import init, { srs } from 'wasm'
 
 function App() {
   useEffect(() => {
     init()
   }, [])
 
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>PC finder</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => inc(count))}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={() => console.log(srs())}>Run</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,46 @@
+use std::rc::Rc;
+use std::str::FromStr;
+
+use bitris_commands::pc_possible::*;
+use bitris_commands::prelude::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn inc(x: usize) -> usize {
-    x + 2
+pub fn srs() -> f64 {
+    // You can also process them all together efficiently with `PcPossibleBulkExecutorBinder`.
+    let mut binder = PcPossibleBulkExecutorBinder::srs();
+
+    // Sets sequences in which you want the PC to be checked.
+    // The following represents 'I****'.
+    let pattern = Pattern::try_from(vec![
+        PatternElement::One(Shape::I),
+        PatternElement::Permutation(ShapeCounter::one_of_each(), 4),
+    ])
+    .expect("Failed to create a pattern");
+    binder.pattern = Rc::from(pattern);
+
+    // The others are the same.
+    let board = Board64::from_str(
+        "
+            ###.....##
+            ###....###
+            ###...####
+            ###....###
+        ",
+    )
+    .expect("Failed to create a board");
+    let height = 4;
+    binder.clipped_board = ClippedBoard::try_new(board, height).expect("Failed to clip");
+    binder.allow_move = AllowMove::Softdrop;
+    binder.allows_hold = true;
+
+    // Finds PCs. If it contains an invalid configuration, an error is returned.
+    let results = binder.try_execute().expect("Failed to execute");
+
+    // You can take the result out of the return value.
+    assert_eq!(results.count_succeed(), 711);
+    assert_eq!(results.count_failed(), 129);
+    assert_eq!(results.count_accepted(), 840);
+
+    results.count_succeed() as f64 / results.count_accepted() as f64
 }
